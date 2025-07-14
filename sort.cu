8ffac865736d242e02cca7b31eeb0f2a101ab1c7
JYYHH <jin511@purdue.edu>
2025-07-14 13:42:48 -0400

    Upgrade the thread usage v0: finish the local sort kernel

--- sort.cu
+++ sort.cu
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int THREADS_PER_BLOCK = 256;
+const int UNIT_PER_BLOCK = THREADS_PER_BLOCK << 1;
 const int THREADS_PER_BLOCK_LOG = 8;
 
 __device__ float _my_get(const float *data, const float *more_data, const int N, const int global_id){
@@ -23,36 +24,44 @@ __global__ void initialize_and_local_sort_kernel(float* data, float* more_data, 
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
     const int block_size = blockDim.x;
-    const int global_id = bid * block_size + tid;
-    __shared__ float shared_data[THREADS_PER_BLOCK];
+    const int global_id = bid * UNIT_PER_BLOCK + tid;
+    __shared__ float shared_data[UNIT_PER_BLOCK];
 
     // initialization for the INF data
     if (global_id >= N)
         shared_data[tid] = INFINITY;
     else
         shared_data[tid] = data[global_id];
+    if (global_id + block_size >= N)
+        shared_data[tid + block_size] = INFINITY;
+    else
+        shared_data[tid + block_size] = data[global_id + block_size];
     __syncthreads();
 
-    // local (in-block)bitonic sort
-    for (int k = 2; k < (N << 1) && k <= block_size; k <<= 1){
-        int in_or_de = (global_id & k) == 0;
+    // local bitonic sort
+    for (int k = 2; k < (N << 1) && k <= UNIT_PER_BLOCK; k <<= 1){
+        int de_or_in = (k == UNIT_PER_BLOCK) ? (bid & 1) : ((tid & (k >> 1)) > 0);
         for (int j = k >> 1; j; j >>= 1){
-            int other_id = tid ^ j;
-            if (tid < other_id){
-                float left_data = shared_data[tid];
-                float right_data = shared_data[other_id];
-                if ((left_data < right_data) ^ in_or_de){
-                    shared_data[tid] = right_data;
-                    shared_data[other_id] = left_data;
-                }
+            int left_id = tid + tid / j * j;
+            int right_id = left_id ^ j;
+            float left_data = shared_data[left_id];
+            float right_data = shared_data[right_id];
+            if ((left_data > right_data) ^ de_or_in){
+                shared_data[left_id] = right_data;
+                shared_data[right_id] = left_data;
             }
-            __syncthreads();
         }
+        __syncthreads();
     }
+                // save data to global memory
     if (global_id >= N)
         more_data[global_id - N] = shared_data[tid];
     else
         data[global_id] = shared_data[tid];
+    if (global_id + block_size >= N)
+        more_data[global_id + block_size - N] = shared_data[tid + block_size];
+    else
+        data[global_id + block_size] = shared_data[tid + block_size];
 }
 
 __global__ void global_sort_single_iteration_kernel(float* data, float* more_data, const int N, const int k, const int j) {
@@ -106,7 +115,7 @@ __global__ void global_sort_multiple_iterations_kernel(float* data, float* more_
 // data is device pointer
 void solve(float* data, int N) {
     const int block_size = THREADS_PER_BLOCK;
-    int block_num = 1, block_num_log = 0;
+    int block_num = 2, block_num_log = 1; // since we change the kernel of local sort
     while ((1 << (THREADS_PER_BLOCK_LOG + block_num_log)) < N) {
         block_num <<= 1;
         block_num_log ++;
@@ -116,13 +125,13 @@ void solve(float* data, int N) {
     // printf("delta: %d, block_num: %d, block_num_log: %d\n", delta, block_num, block_num_log);
     if (delta)
         cudaMalloc(&more_data, (delta * sizeof(float)));
-    initialize_and_local_sort_kernel<<<block_num, block_size>>>(data, more_data, N);
+    initialize_and_local_sort_kernel<<<(block_num >> 1), block_size>>>(data, more_data, N);
     cudaDeviceSynchronize();
     // after that local sort, the first block will be in ascending order, second in descending order, third in ascending order, etc...
 
     if (block_num_log){
         // We need to sort among blocks
-        for (int k = block_size << 1; k < (N << 1); k <<= 1){
+        for (int k = block_size << 2; k < (N << 1); k <<= 1){ // since we change the kernel of local sort
             for (int j = k >> 1; j >= block_size; j >>= 1){
                 global_sort_single_iteration_kernel<<<block_num, block_size>>>(data, more_data, N, k, j);
                 cudaDeviceSynchronize();
