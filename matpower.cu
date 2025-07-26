6f376e02b918d798502da9945864021536d6b1fb
JYYHH <jin511@purdue.edu>
2025-07-26 01:37:28 -0400

    Add a small optimiz on bmm kernel & V0 for matpower (but leetGPU shows there is numerical error)

--- matpower.cu
+++ matpower.cu
@@ -32,7 +32,6 @@ __global__ void MatrixMultiplyKernel(
         }
     }
 
-    const int batch_id = blockIdx.z;
     const int row_offset = threadIdx.y;
     const int col_offset = threadIdx.x;
     const int reduce_dim = (K + K_TILE_SIZE - 1) / K_TILE_SIZE;
@@ -49,8 +48,8 @@ __global__ void MatrixMultiplyKernel(
                 const int col_B = blockIdx.x * blockDim.x * LOCAL_TILE_LENGTH + col_shared;
                 const int col_A = iter * K_TILE_SIZE + col_shared;
                 const int row_B = iter * K_TILE_SIZE + row_shared;
-                A_shared[row_shared][col_shared] = (row_A < M && col_A < K) ? A[batch_id * M * K + row_A * K + col_A] : 0.0f;
-                B_shared[row_shared][col_shared] = (row_B < K && col_B < N) ? B[batch_id * K * N + row_B * N + col_B] : 0.0f;
+                A_shared[row_shared][col_shared] = (row_A < M && col_A < K) ? A[row_A * K + col_A] : 0.0f;
+                B_shared[row_shared][col_shared] = (row_B < K && col_B < N) ? B[row_B * N + col_B] : 0.0f;
             }
         }
         __syncthreads();
@@ -78,65 +77,72 @@ __global__ void MatrixMultiplyKernel(
             const int row = blockIdx.y * blockDim.y * LOCAL_TILE_LENGTH + i * blockDim.y + row_offset;
             const int col = blockIdx.x * blockDim.x * LOCAL_TILE_LENGTH + j * blockDim.x + col_offset;
             if (row < M && col < N){
-                C[batch_id * M * N + row * N + col] = matmul_local[i * LOCAL_TILE_LENGTH + j];
+                C[row * N + col] = matmul_local[i * LOCAL_TILE_LENGTH + j];
             }
         }
     }
 }
 
-// A, B, and C are device pointers
-void solve(const float* A, const float* B, float* C, int BATCH, int M, int N, int K) {
-    dim3 block(M_TILE_SIZE / LOCAL_TILE_LENGTH, N_TILE_SIZE / LOCAL_TILE_LENGTH);
-    dim3 grid((M + M_TILE_SIZE - 1) / M_TILE_SIZE, (N + N_TILE_SIZE - 1) / N_TILE_SIZE, BATCH);
-    // cudaMemset(C, 0, BATCH * M * N * sizeof(float));
-    MatrixMultiplyKernel<<<grid, block>>>(A, B, C, M, K, N);
-    cudaDeviceSynchronize();
-}
-
-int main() {
-    const int BATCH = 16;
-    const int M = 1024;
-    const int N = 1024;
-    const int K = 1024;
-    float* A = (float*)malloc(BATCH * M * K * sizeof(float));
-    float* B = (float*)malloc(BATCH * K * N * sizeof(float));
-    float* C = (float*)malloc(BATCH * M * N * sizeof(float));
-    memset(A, 0, BATCH * M * K * sizeof(float));
-    memset(B, 0, BATCH * K * N * sizeof(float));
-    memset(C, 0, BATCH * M * N * sizeof(float));
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < K; j++) {
-            A[i * K + i] = 1.0f;
-        }   
+void solve(const float* input, float* output, int N, int P) {
+    // special case
+    if (P == 1){
+        cudaMemcpy(output, input, N * N * sizeof(float), cudaMemcpyDeviceToDevice);
+        return;
     }
-    for (int i = 0; i < K; i++) {
-        for (int j = 0; j < N; j++) {
-            B[i * N + j] = 1.0f;
+    // general case, and P >= 2
+    float* mid_matrix;
+    cudaMalloc((void**)&mid_matrix, N * N * sizeof(float));
+    dim3 block(N_TILE_SIZE / LOCAL_TILE_LENGTH, N_TILE_SIZE / LOCAL_TILE_LENGTH);
+    dim3 grid((N + N_TILE_SIZE - 1) / N_TILE_SIZE, (N + N_TILE_SIZE - 1) / N_TILE_SIZE);
+
+    int p = 0, where_data = 0, used = 0; // where_data == 0 means current power is in output
+    while ((1 << p) <= P)
+        p ++;
+    p --;
+    // now p >= 1
+    for (int i = p - 1; i >= 0; i--){
+        if (P & (1 << i)){
+            if (used){
+                if (where_data){
+                    MatrixMultiplyKernel<<<grid, block>>>(input, mid_matrix, output, N, N, N);
+                    MatrixMultiplyKernel<<<grid, block>>>(output, output, mid_matrix, N, N, N); 
+                }
+                else{
+                    MatrixMultiplyKernel<<<grid, block>>>(input, output, mid_matrix, N, N, N);
+                    MatrixMultiplyKernel<<<grid, block>>>(mid_matrix, mid_matrix, output, N, N, N); 
+                }
+            }
+            else{
+                if (where_data){
+                    MatrixMultiplyKernel<<<grid, block>>>(input, input, output, N, N, N);
+                    MatrixMultiplyKernel<<<grid, block>>>(input, output, mid_matrix, N, N, N);   
+                }
+                else{
+                    MatrixMultiplyKernel<<<grid, block>>>(input, input, mid_matrix, N, N, N);
+                    MatrixMultiplyKernel<<<grid, block>>>(input, mid_matrix, output, N, N, N);
+                }
+                used = 1;
+            }
+        }
+        else{
+            if (used){
+                if (where_data)
+                    MatrixMultiplyKernel<<<grid, block>>>(mid_matrix, mid_matrix, output, N, N, N);
+                else
+                    MatrixMultiplyKernel<<<grid, block>>>(output, output, mid_matrix, N, N, N);
+            }
+            else{
+                if (where_data)
+                    MatrixMultiplyKernel<<<grid, block>>>(input, input, output, N, N, N);
+                else
+                    MatrixMultiplyKernel<<<grid, block>>>(input, input, mid_matrix, N, N, N);
+                used = 1;
+            }
+            where_data ^= 1;
         }
     }
-    float* d_A, *d_B, *d_C;
-    cudaMalloc((void**)&d_A, BATCH * M * K * sizeof(float));
-    cudaMalloc((void**)&d_B, BATCH * K * N * sizeof(float));
-    cudaMalloc((void**)&d_C, BATCH * M * N * sizeof(float));
-    cudaMemcpy(d_A, A, BATCH * M * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, BATCH * K * N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_C, C, BATCH * M * N * sizeof(float), cudaMemcpyHostToDevice);   
-    clock_t start = clock();
-    solve(d_A, d_B, d_C, BATCH, M, N, K);
-    clock_t end = clock();
-    printf("Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    cudaMemcpy(C, d_C, BATCH * M * N * sizeof(float), cudaMemcpyDeviceToHost);
-    // for (int i = 0; i < M; i++) {
-    //     for (int j = 0; j < N; j++) {
-    //         printf("%f ", (float)C[i * N + j]);
-    //     }
-    //     printf("\n");
-    // }
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-    free(A);
-    free(B);
-    free(C);
-    return 0;
-}
+    if (where_data)
+        cudaMemcpy(output, mid_matrix, N * N * sizeof(float), cudaMemcpyDeviceToDevice);
+    cudaFree(mid_matrix);
+    cudaDeviceSynchronize();
+} 
